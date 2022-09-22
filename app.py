915aedbc80083b283ddeff9862c2ca45d27faed4
Yige <tjzhangyige@gmail.com>
2022-09-21 21:33:53 -0600

    add nav bars and images

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template, request
+from flask import send_from_directory
 
 app = Flask(__name__) ## turn the current file into a flask app
 
 @app.route("/") # create a route
 def home():
-    return render_template('home.html')
+    return render_template('about.html')
+
+@app.route('/illustration')
+def illustration():
+    return render_template('illustration.html')
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+@app.route('/game/index.html')
+def game_1():
+    return render_template('/game/index.html')
 
 
-# @app.route('/greet', methods = ["POST"])
-# def greet():
-#     name = request.form.get("name", "world") # give a default name if it is not input by user
-#     return render_template("greet.html", name=name)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
