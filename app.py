a75faaef1e35b51d185803eb815253e7d5c5f2c2
Yige <tjzhangyige@gmail.com>
2022-09-25 13:12:43 -0600

    add log

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import logging
+
+from tkinter import W
 from flask import Flask, render_template, request
-from flask import send_from_directory
 
 app = Flask(__name__) ## turn the current file into a flask app
+
 
 @app.route("/") # create a route
 def home():
@@ -15,14 +18,15 @@ def resume():
 def illustration():
     return render_template('illustration.html')
 
+
 @app.route('/game')
 def game():
     return render_template('game.html')
 
+
 @app.route('/cat')
 def cat():
     return render_template('/cat.html')
-
 
 
 @app.route('/contact')
@@ -30,4 +34,5 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='web.log',level=logging.DEBUG)
     app.run(debug = True)
